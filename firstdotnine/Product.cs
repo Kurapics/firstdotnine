7cebc92494fbb1b95a9f93e241c56cba7b458e9e
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:50:15 +0200

    Добавьте файлы проекта.

---
+++ firstdotnine/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstdotnine
+{
+    internal class Product
+    {
+        public double Weight { get; set; }
+        public double CaloriesPer100g { get; set; }
+
+        public Product(double weight, double caloriesPer100g)
+        {
+            Weight = weight;
+            CaloriesPer100g = caloriesPer100g;
+        }
+
+        public double GetAllCalories()
+        {
+            return Weight / CaloriesPer100g;
+        }
+    }
+}
---
+++ firstdotnine/Program.cs
@@ -0,0 +1,16 @@
+﻿using firstdotnine;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите вес продукта (г): ");
+        double Weight = double.Parse(Console.ReadLine());
+        
+        Console.Write("Введите калорийность на 100г продукта: ");
+        double CaloriesPer100g = double.Parse(Console.ReadLine());
+
+        Product product = new(Weight, CaloriesPer100g);
+        Console.WriteLine("Общая калорийность продукта: " + product.GetAllCalories());
+    }
+}
